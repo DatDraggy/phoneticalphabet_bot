c4bb8e2d4308562aff77db811e248c28a49f874c
DatDraggy <spamme@kieran.de>
2019-06-24 15:34:49 +0200

    dont need no switch

--- webhook.php
+++ webhook.php
@@ -16,55 +16,38 @@ if (isset($data['message']['text'])) {
   $text = preg_replace('/[^\wÖÄÜß]/', '', strtoupper($data['message']['text']));
 }
 
-if (isset($text)) {
-  if (substr($text, '0', '1') == '/') {
-    $messageArr = explode(' ', $text);
-    $command = explode('@', $messageArr[0])[0];
-    if ($messageArr[0] == '/start' && isset($messageArr[1])) {
-      $command = '/' . $messageArr[1];
-    }
-  } else {
-    die();
-  }
+if ($text == '/start') {
+  sendMessage($chatId, 'Send me text and I will convert it into the phonetic alphabet.');
 
-  $command = strtolower($command);
+} else {
+  $characters = str_split($text);
 
-  switch ($command) {
-    case '/start':
-      sendMessage($chatId, 'Send me text and I will convert it into the phonetic alphabet.');
-      break;
-    default:
-      $characters = str_split($text);
+  $i = 0;
 
-      $i = 0;
+  $converted = '';
 
-      $converted = '';
-
-      while ($i < count($characters)) {
-        if (is_int($characters[$i]) && $characters[$i] != 0) {
-          //Figure
-          $zeros = 0;
-          for ($x = 1; $x < 3; $x++) {
-            if ($characters[$i + $x] == 0) {
-              $zeros += 1;
-            }
-          }
-          if ($zeros == 2) {
-            $converted .= $alphabet[$characters[$i]] . ' HUNDRED';
-            $i += 2;
-          } else if ($zeros == 3) {
-            $converted .= $alphabet[$characters[$i]] . ' THOUSAND';
-            $i += 3;
-          }
-        } else {
-          //Character
-          $converted .= $alphabet[$characters[$i]];
+  while ($i < count($characters)) {
+    if (is_int($characters[$i]) && $characters[$i] != 0) {
+      //Figure
+      $zeros = 0;
+      for ($x = 1; $x < 3; $x++) {
+        if ($characters[$i + $x] == 0) {
+          $zeros += 1;
         }
-
-        $i += 1;
-        $converted .= ' ';
       }
+      if ($zeros == 2) {
+        $converted .= $alphabet[$characters[$i]] . ' HUNDRED';
+        $i += 2;
+      } else if ($zeros == 3) {
+        $converted .= $alphabet[$characters[$i]] . ' THOUSAND';
+        $i += 3;
+      }
+    } else {
+      //Character
+      $converted .= $alphabet[$characters[$i]];
+    }
 
-
+    $i += 1;
+    $converted .= ' ';
   }
 }
