98ca12f4c776d213ee3d47f37e5d579ea58c121b
DatDraggy <spamme@kieran.de>
2019-07-16 12:43:18 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -43,7 +43,7 @@ if ($text == '/start') {
     sendMessage($chatId, $converted);
     logMessage($senderUserId, $chatId, $text, $converted);
   } else {
-    $text = trim(preg_replace('/[\n\r]/', ' ', $text));
+    $text = preg_replace('/[\n\r]/', ' ', $text);
     $text = str_replace(['ö', 'Ö'], 'OE', str_replace(['ä', 'Ä'], 'AE', str_replace(['ü', 'Ü'], 'UE', $text)));
     $text = preg_replace('/[^\w.ß ]/', '', strtoupper($text));
     if (empty($text)) {
